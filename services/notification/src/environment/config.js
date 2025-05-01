16e350ce8ac4823fe0ca04dc110d50cdd8bc9a51
Jack-Chagarr <netamijr@gmail.com>
2025-05-01 23:31:47 +0900

    Add skeleton

--- services/notification/src/environment/config.js
+++ services/notification/src/environment/config.js
@@ -3,10 +3,10 @@ const config = {
   messagebus: process.env.MESSAGE_BUS || 'amqp://guest:guest@localhost',
   environment: process.env.ENVIRONMENT || 'dev',
   email: {
-    service: process.env.EMAIL_SERVICE || '',
-    username: process.env.EMAIL_ID || '',
-    password: process.env.EMAIL_PASSWORD || '',
-    adminEmailID: process.env.ADMIN_EMAIL || '',
+    service: process.env.EMAIL_SERVICE || 'gmail',
+    username: process.env.EMAIL_ID || '이메일 주소',
+    password: process.env.EMAIL_PASSWORD || '앱 패스워드',
+    adminEmailID: process.env.ADMIN_EMAIL || '다른 이메일 주소',
   },
 };
 
